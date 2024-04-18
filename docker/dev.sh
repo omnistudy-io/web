7dff8fce03bab6ea06db2ab70aee0e6b0d2ed3e9
grude013 <grude013@umn.edu>
2024-04-18 01:31:16 -0500

    changed port for web

--- docker/dev.sh
+++ docker/dev.sh
@@ -3,4 +3,4 @@ sudo chown -R $(whoami) ~/.docker
 # Build
 docker build -t omnistudy/web:dev -f ./docker/Dockerfile.dev .
 # Run
-docker run -dp 3000:3000 --name web omnistudy/web:dev
+docker run -dp 3005:3005 --name web omnistudy/web:dev
